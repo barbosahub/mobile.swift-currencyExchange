037db36ac8da2ed348daf1a6b3dc20dbf035b944
Barbosahub <rui_barbosa.junior@hotmail.com>
2022-10-09 18:14:05 -0300

    Initial Commit

---
+++ currencyConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  currencyConverter
+//
+//  Created by Rui Barbosa da Silva Junior on 09/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
